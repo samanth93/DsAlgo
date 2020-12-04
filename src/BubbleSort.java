af9b8c63e35030d8ada6e83360bf97be70018633
samanth93 <samanth.sid@gmail.com>
2020-12-04 17:53:07 -0500

    Selection Sort

--- src/BubbleSort.java
+++ src/BubbleSort.java
@@ -4,8 +4,8 @@ public class BubbleSort {
 	public static void main(String[] args) {
 		int[] arr = {20, 35, -15, 7, 55, 1, -22};
 		int unSortedPartionIndex = arr.length;
-		for(int k=unSortedPartionIndex;k>-1;k--) {
-			for(int i=0;i<k-1;i++) {
+		for(int k=unSortedPartionIndex-1;k>-1;k--) {
+			for(int i=0;i<k;i++) {
 				swapValues(arr, i, i+1);
 			}	
 		}
---
+++ src/SelectionSort.java
@@ -0,0 +1,25 @@
+
+public class SelectionSort {
+	public static void main(String[] args) {
+		int[] arr = {20, 35, -15, 7, 55, 1, -22};
+		for(int lastUnsortedIndex=arr.length-1;lastUnsortedIndex>0;lastUnsortedIndex--) {
+			int highestIndex = 0;
+//			here below we need <= not only < as bubble sort because in bubble sort we will compare i and i+1, not like here
+			for(int i=1;i<=lastUnsortedIndex;i++) {
+				if(arr[highestIndex] < arr[i]) {
+					highestIndex = i;
+				}
+			}
+			swapItems(arr, highestIndex, lastUnsortedIndex);
+		}
+		for(int l=0;l<arr.length;l++) {
+			System.out.print("\t"+arr[l]);
+		}
+		
+	}
+	public static void swapItems(int[] arr,int i ,int j) {
+		int temp = arr[i];
+		arr[i] = arr[j];
+		arr[j] = temp;
+	}
+}
