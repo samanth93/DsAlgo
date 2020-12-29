68057003833471fce11b9ce38770ff8597d555e0
samanth93 <samanth.sid@gmail.com>
2020-12-28 20:03:12 -0500

    HashMap-JDK

--- src/HashTableChaining/Main.java
+++ src/HashTableChaining/Main.java
@@ -1,7 +1,10 @@
 package HashTableChaining;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 import java.util.Vector;
 
 public class Main {
@@ -11,13 +14,17 @@ public class Main {
 		Employee johnDoe = new Employee("John", "Doe", 4567);
 		Employee marySmith = new Employee("Mary", "Smith", 22);
 		Employee mikeWilson = new Employee("Mike", "Wilson", 3245);
-		Employee billEnd = new Employee("Bill", "End", 78);		
-		ChainedHashTable ht = new ChainedHashTable();
-		ht.put("Jones", janeJones);
-		ht.put("Doe", johnDoe);
-		ht.put("Wilson", mikeWilson);
-		ht.put("Smith", marySmith);
-		ht.put("End", billEnd);
-		ht.printHashTable();
+		Map<String, Employee> hashMap = new HashMap<String, Employee>();
+		hashMap.put("Jones", janeJones);
+		hashMap.put("Doe", johnDoe);
+		hashMap.put("Smith", marySmith);
+		System.out.println(hashMap.containsKey("Doe"));
+		System.out.println(hashMap.containsValue(johnDoe));
+//		Iterator<Employee> iterator = hashMap.values().iterator();
+//		while(iterator.hasNext()) {
+//			System.out.println(iterator.next());
+//		}
+		hashMap.putIfAbsent("Doe", mikeWilson);
+		hashMap.forEach((k, v)->System.out.println("Key = "+k+", Employee = "+v));
 	}
 }
