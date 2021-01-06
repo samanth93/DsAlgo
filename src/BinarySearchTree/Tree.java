764154e2fc87aa285e36b6f103d442acbcde5f24
samanth93 <samanth.sid@gmail.com>
2021-01-05 19:39:27 -0500

    Min and Max Values

--- src/BinarySearchTree/Tree.java
+++ src/BinarySearchTree/Tree.java
@@ -10,6 +10,22 @@ public class Tree {
 		}
 	}
 	
+	public int min() {
+		if(root == null) {
+			return Integer.MIN_VALUE;
+		}else {
+			return root.min();
+		}
+	}
+	
+	public int max() {
+		if(root == null) {
+			return Integer.MAX_VALUE;
+		}else {
+			return root.max();
+		}
+	}
+	
 	public void traverseInOrder() {
 		if(root !=null) {
 			root.traverseInOrder();
