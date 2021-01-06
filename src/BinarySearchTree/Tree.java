4f2f709d7ec646e5266d69a062edd2e8bf233f23
samanth93 <samanth.sid@gmail.com>
2021-01-06 18:49:41 -0500

    Delete 1 st and 2nd case

--- src/BinarySearchTree/Tree.java
+++ src/BinarySearchTree/Tree.java
@@ -38,4 +38,28 @@ public class Tree {
 		}
 		return null;
 	}
+	
+	public void delete(int value) {
+//		we are passing the root of the subtree, and the value we want to delete
+		root = delete(root, value);
+	}
+	
+	private TreeNode delete(TreeNode subtreeRoot, int value) {
+		if(subtreeRoot == null) {
+			return subtreeRoot;
+		}
+		if(value < subtreeRoot.getData()) {
+			subtreeRoot.setLeftChild(delete(subtreeRoot.getLeftChild(), value));
+		}else if(value > subtreeRoot.getData()) {
+			subtreeRoot.setRightChild(delete(subtreeRoot.getRightChild(), value));
+		}else {
+//			cases 1 and 2: node to delete has 1 or 2 children
+			if(subtreeRoot.getLeftChild()==null) {
+				return subtreeRoot.getRightChild();
+			}else if(subtreeRoot.getRightChild()==null) {
+				return subtreeRoot.getLeftChild();
+			}
+		}
+		return subtreeRoot;
+	}
 }
